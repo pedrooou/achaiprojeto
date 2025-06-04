6fff1c4c43725a7fa2e784bddf6ae81983eee8f9
Estevao767 <vale96940@gmail.com>
2025-06-03 23:52:35 -0300

    modificações feitas hojepedro e eu

---
+++ www/tabvendedor/telasVendedor/tela4/ramificacoes/tela1NossoProjeto/nossoprojeto.js
@@ -0,0 +1,4 @@
+function politica() {
+    window.parent.document.getElementById('conteudo').src = '../telasVendedor/tela4/ramificacoes/tela1NossoProjeto/quemsomos/quemsomos.html';
+
+}
---
+++ www/tabvendedor/telasVendedor/tela4/tela4vendedor.js
@@ -0,0 +1,3 @@
+function mudarIframe() {
+   window.parent.document.getElementById('conteudo').src = '../telasVendedor/tela4/ramificacoes/tela1NossoProjeto/nossoprojeto.html';
+}
