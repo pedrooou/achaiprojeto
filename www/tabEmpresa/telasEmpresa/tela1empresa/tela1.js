aed3c975d469183de5f450d12839a5aa96cd15b8
Estevao767 <vale96940@gmail.com>
2025-06-22 11:14:53 -0300

    eu programei a parte do icone

---
+++ www/tabEmpresa/telasEmpresa/tela1empresa/tela1.js
@@ -0,0 +1,10 @@
+function sairDaConta() {
+  localStorage.removeItem('tipoConta');
+  window.top.location.href = "../../../index.html";
+}
+function editarMotorista() {
+  window.location.href = 'ramificacoes/perfil.html'; // alterado para perfil.html
+}
+function irParaProjeto() {
+  window.location.href = '../../../../www/tabvendedor/telasVendedor/tela4/ramificacoes/tela1NossoProjeto/quemsomos.html';
+}
